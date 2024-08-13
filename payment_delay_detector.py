150980cbbba6f3106cfc43de4df98e6ff8178b22
arypr <123067276+arypr@users.noreply.github.com>
2024-08-13 18:24:55 +0700

    Update payment_delay_detector.py

--- payment_delay_detector.py
+++ payment_delay_detector.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 import seaborn as sns
+import catboost
 import matplotlib.pyplot as plt
 from catboost import CatBoostClassifier
 from sklearn.preprocessing import LabelEncoder
